f2d37cedb7a16274b53e6d463ca13cb82b90d031
Shuowang He <sh915@cam.ac.uk>
2019-11-11 14:33:15 +0000

    Output elapsed time and mass reading to a csv

--- Serial_reader.py
+++ Serial_reader.py
@@ -1,6 +1,18 @@
 import serial
+import time
+import csv
+
 arduino = serial.Serial('/dev/cu.usbmodem14201',38400)
+start_time = time.time()
+array = []
+i=0
 while True:
     data = arduino.readline()[:-2]
     if data:
-        print(data)
+        elapsed_time = time.time()-start_time
+        mass = float(data[-9:-3])
+        array.append([elapsed_time,mass])
+        print(elapsed_time,mass)
+        with open('Output.csv','a',) as newFile:
+            newFileWriter = csv.writer(newFile)
+            newFileWriter.writerow([elapsed_time,mass])
